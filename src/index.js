aef23de90af26b22340ac19bc3789302f51cdca7
firzok-iai <firzok.nadeem@incline-ai.com>
2019-10-21 18:21:23 +0500

    Changes to page header

--- src/index.js
+++ src/index.js
@@ -24,28 +24,28 @@ import "assets/scss/blk-design-system-react.scss?v=1.0.0";
 import "assets/demo/demo.css";
 
 import Index from "views/Index.jsx";
-import LandingPage from "views/examples/LandingPage.jsx";
-import RegisterPage from "views/examples/RegisterPage.jsx";
-import ProfilePage from "views/examples/ProfilePage.jsx";
+import LandingPage from "views/LandingPage.jsx";
+import RegisterPage from "views/RegisterPage.jsx";
+import ProfilePage from "views/ProfilePage.jsx";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route path="/components" render={props => <Index {...props} />} />
-      <Route
-        path="/landing-page"
-        render={props => <LandingPage {...props} />}
-      />
-      <Route
-        path="/register-page"
-        render={props => <RegisterPage {...props} />}
-      />
-      <Route
-        path="/profile-page"
-        render={props => <ProfilePage {...props} />}
-      />
-      <Redirect from="/" to="/components" />
-    </Switch>
-  </BrowserRouter>,
-  document.getElementById("root")
+	<BrowserRouter>
+		<Switch>
+			<Route path="/home" render={props => <Index {...props} />} />
+			<Route
+				path="/landing-page"
+				render={props => <LandingPage {...props} />}
+			/>
+			<Route
+				path="/register-page"
+				render={props => <RegisterPage {...props} />}
+			/>
+			<Route
+				path="/profile-page"
+				render={props => <ProfilePage {...props} />}
+			/>
+			<Redirect from="/" to="/home" />
+		</Switch>
+	</BrowserRouter>,
+	document.getElementById("root")
 );
--- src/views/Index.jsx
+++ src/views/Index.jsx
@@ -46,23 +46,23 @@ class Index extends React.Component {
 		return (
 			<>
 				<IndexNavbar />
-				{/* <div className="wrapper">
-          <PageHeader />
-          <div className="main">
-            <Basics />
-            <Navbars />
-            <Tabs />
-            <Pagination />
-            <Notifications />
-            <Typography />
-            <JavaScript />
-            <NucleoIcons />
-            <Signup />
-            <Examples />
-            <Download />
-          </div>
-          <Footer />
-        </div> */}
+				<div className="wrapper">
+					<PageHeader />
+					{/* <div className="main">
+						<Basics />
+						<Navbars />
+						<Tabs />
+						<Pagination />
+						<Notifications />
+						<Typography />
+						<JavaScript />
+						<NucleoIcons />
+						<Signup />
+						<Examples />
+						<Download />
+					</div> */}
+					<Footer />
+				</div>
 			</>
 		);
 	}
