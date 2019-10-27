27f15acaf5306a4adc9b70345bb9c02b7b046a65
Firzok Nadeem <firzok.nadeem@gmail.com>
2019-10-27 20:23:03 +0500

    Updates

---
+++ precache-manifest.75c5437b8e62d58050a79cdc1eca5c59.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "9f2045b40ed255448fdd2810bed50697",
+    "url": "/static/media/bentley.9f2045b4.png"
+  },
+  {
+    "revision": "0b3a9c7036bbfa35c4c0",
+    "url": "/static/js/main.0b3a9c70.chunk.js"
+  },
+  {
+    "revision": "b1e01ebbb0d098277dcc97759bc3f402",
+    "url": "/static/media/path1.b1e01ebb.png"
+  },
+  {
+    "revision": "13a2068d81c76740ae3aa2a7fc1586bd",
+    "url": "/static/media/Firzok.13a2068d.jpg"
+  },
+  {
+    "revision": "a70167ea1491987c50049961a9e2e45f",
+    "url": "/static/media/path3.a70167ea.png"
+  },
+  {
+    "revision": "8cc45664f9570ba23d66d8c59e998ab0",
+    "url": "/static/media/aai.8cc45664.svg"
+  },
+  {
+    "revision": "ceb241f05718fb1311f8810eea959803",
+    "url": "/static/media/path5.ceb241f0.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "eb0ae9b6bc9b54840a9255415eadec00",
+    "url": "/static/media/github-logo.eb0ae9b6.png"
+  },
+  {
+    "revision": "4a8c23476a7c20c5bee2a752a6f96e9e",
+    "url": "/static/media/instagram.4a8c2347.png"
+  },
+  {
+    "revision": "3dae2e180b6e7235b4bc033f3f6920de",
+    "url": "/static/media/NU-logo.3dae2e18.png"
+  },
+  {
+    "revision": "01899307116a937c66a0eb4b3927b631",
+    "url": "/static/media/blob.01899307.png"
+  },
+  {
+    "revision": "5d37564ea3a7d47bfc73",
+    "url": "/static/js/2.5d37564e.chunk.js"
+  },
+  {
+    "revision": "7e4af5493e6fdb9c58c98e66d30aceec",
+    "url": "/index.html"
+  }
+];
