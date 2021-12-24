998f3e22035effbde514fc068e5fe417cacf4636
Firzok Nadeem <firzok.nadeem@gmail.com>
2021-12-24 01:55:37 +0100

    deploy changes

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:firzok/firzok.github.io master:gh-pages
+git push -f git@github.com:firzok/firzok.github.io dev2:gh-pages
 cd -
